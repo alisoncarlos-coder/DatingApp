5de2efc40a223b7c8e477b2cc8e248160b7ff2ce
alisoncarlos-coder <78183463+alisoncarlos-coder@users.noreply.github.com>
2024-02-26 21:24:41 -0300

    Adicionado funcionalidades de Registro e Login de usuários

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,19 @@
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddAppServices(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
 
 var app = builder.Build();
 
@@ -20,8 +23,7 @@ app.UseCors(corsPolicyBuilder => corsPolicyBuilder
                                                 .AllowAnyMethod()
                                                 .WithOrigins("https://localhost:4200"));
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
